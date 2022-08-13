ecc0892bd00af4fc16373bb16b4d7fe0bc3f8bd7
Harshit639 <82362058+Harshit639@users.noreply.github.com>
2022-08-13 12:13:54 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const express= require("express")
+const app =express()
+require('dotenv').config()
+const { MongoClient } = require('mongodb')
+const url= process.env.MONGO_URL
+const client =new MongoClient(url)
+const dbName = 'books'
+
+const db = client.db(dbName)
+const collection=db.collection('books')
+app.use("/static",express.static("static"))
+app.get("/books", async function(req,res){
+    let list= await collection.find({}).toArray();
+    res.json(list)
+})
+app.listen(3000)
